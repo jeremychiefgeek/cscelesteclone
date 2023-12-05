25d9f36fb34291365f315558292ca3a8e0f67ddd
Jeremy Evans <jeremy@chiefgeek.io>
2023-12-05 18:40:53 -0500

    Initial commit

---
+++ csceleste/Program.cs
@@ -0,0 +1,46 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+namespace cscleste
+{
+    public static class Program
+    {
+        private static IWindow _window;
+
+        public static void Main(string[] args)
+        {
+            WindowOptions options = WindowOptions.Default with
+            {
+                Size = new Vector2D<int>(800, 600),
+                Title = "Celeste"
+            };
+
+            _window = Window.Create(options);
+
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+
+            _window.Run();
+        }
+
+        public static void OnLoad()
+        {
+            IInputContext input = _window.CreateInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+                input.Keyboards[i].KeyDown += KeyDown;
+        }
+
+        public static void OnUpdate(double deltaTime) { }
+
+        public static void OnRender(double deltaTime) { }
+
+        private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+        {
+            if (key == Key.Escape)
+                _window.Close();
+        }
+    }
+}
