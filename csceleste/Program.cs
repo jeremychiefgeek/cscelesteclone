4b4e19a1a2f9c3166ce388c878bdf449377fb330
jeremyevans <jeremy@chiefgeek.io>
2023-12-05 21:00:50 -0500

    abstractions

--- csceleste/Program.cs
+++ csceleste/Program.cs
@@ -8,47 +8,121 @@ namespace cscleste
 {
     public static class Program
     {
-        private static IWindow _window;
-        private static GL _gl;
+        private static IWindow window;
+        private static GL Gl;
 
-        public static void Main(string[] args)
+        //Our new abstracted objects, here we specify what the types are.
+        private static BufferObject<float> Vbo;
+        private static BufferObject<uint> Ebo;
+        private static VertexArrayObject<float, uint> Vao;
+
+        public static Texture Texture;
+        private static Shader Shader;
+
+        private static readonly float[] Vertices =
         {
-            WindowOptions options = WindowOptions.Default with
-            {
-                Size = new Vector2D<int>(800, 600),
-                Title = "Celeste"
-            };
+            //X    Y      Z     S    T
+            0.5f,
+            0.5f,
+            0.0f,
+            1.0f,
+            0.0f,
+            0.5f,
+            -0.5f,
+            0.0f,
+            1.0f,
+            1.0f,
+            -0.5f,
+            -0.5f,
+            0.0f,
+            0.0f,
+            1.0f,
+            -0.5f,
+            0.5f,
+            0.5f,
+            0.0f,
+            0.0f
+        };
 
-            _window = Window.Create(options);
+        private static readonly uint[] Indices = { 0, 1, 3, 1, 2, 3 };
 
-            _window.Load += OnLoad;
-            _window.Update += OnUpdate;
-            _window.Render += OnRender;
+        private static void Main(string[] args)
+        {
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+            window = Window.Create(options);
 
-            _window.Run();
+            window.Load += OnLoad;
+            window.Render += OnRender;
+            window.Closing += OnClose;
+
+            window.Run();
+
+            window.Dispose();
         }
 
-        public static void OnLoad()
+        private static void OnLoad()
         {
-            IInputContext input = _window.CreateInput();
+            IInputContext input = window.CreateInput();
             for (int i = 0; i < input.Keyboards.Count; i++)
+            {
                 input.Keyboards[i].KeyDown += KeyDown;
+            }
 
-            _gl = _window.CreateOpenGL();
-            _gl.ClearColor(Color.CornflowerBlue);
+            Gl = GL.GetApi(window);
+
+            //Instantiating our new abstractions
+            Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
+            Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
+            Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
+
+            //Telling the VAO object how to lay out the attribute pointers
+            Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 5, 0);
+            Vao.VertexAttributePointer(1, 2, VertexAttribPointerType.Float, 5, 3);
+
+            Shader = new Shader(Gl, "shader.vert", "shader.frag");
+
+            Texture = new Texture(Gl, "silk.png");
         }
 
-        public static void OnUpdate(double deltaTime) { }
-
-        public static void OnRender(double deltaTime)
+        private static unsafe void OnRender(double obj)
         {
-            _gl.Clear(ClearBufferMask.ColorBufferBit);
+            Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+            //Binding and using our VAO and shader.
+            Vao.Bind();
+            Shader.Use();
+
+            Texture.Bind(TextureUnit.Texture0);
+
+            //Setting a uniform.
+            Shader.SetUniform("uTexture", 0);
+
+            Gl.DrawElements(
+                PrimitiveType.Triangles,
+                (uint)Indices.Length,
+                DrawElementsType.UnsignedInt,
+                null
+            );
         }
 
-        private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+        private static void OnClose()
+        {
+            //Remember to dispose all the instances.
+            Vbo.Dispose();
+            Ebo.Dispose();
+            Vao.Dispose();
+            Shader.Dispose();
+            Texture.Dispose();
+        }
+
+        private static void KeyDown(IKeyboard keyboard, Key key, int arg3)
         {
             if (key == Key.Escape)
-                _window.Close();
+            {
+                window.Close();
+            }
         }
     }
 }
